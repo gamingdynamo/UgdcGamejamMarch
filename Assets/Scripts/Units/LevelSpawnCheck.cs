eb3c1630e8fcd04a7840ebf8f66a44001340242d
Anurag Dubey <anuragdubeyGames@gmail.com>
2023-03-07 17:00:00 +0530

    BasicEndlessLevelGenerator

--- Assets/Scripts/Units/LevelSpawnCheck.cs
+++ Assets/Scripts/Units/LevelSpawnCheck.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class LevelSpawnCheck : MonoBehaviour
 {
     LevelGenerator levelGenerator;
+    [SerializeField] private GameObject Box;
+    [SerializeField] private float MinX, MinZ;
+    [SerializeField] private float MaxX, MaxZ;
     void Start()
     {
         levelGenerator = FindObjectOfType<LevelGenerator>();
+        SpawnRandomBox();
     }
-
+    void SpawnRandomBox()
+    {
+        Instantiate(Box,transform.parent.position + new Vector3(Random.Range(MinX, MaxX),1.75f,Random.Range(MinZ, MaxZ)), Quaternion.identity);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.TryGetComponent<Movement>(out Movement player))
